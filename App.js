fba710840f8b88546de515ddc9d1acd86492440d
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2021-11-08 14:45:54 +0200

    Create simple calculator app

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [numA, setNumA] = useState(0);
+  const [numB, setNumB] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const addition = () => {
+    setResult(parseFloat(numA) + parseFloat(numB));
+  };
+
+  const subtraction = () => {
+    setResult(parseFloat(numA) - parseFloat(numB));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <Text>Result: {result}</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={numA => setNumA(numA)}
+          value={String(numA)}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={numB => setNumB(numB)}
+          value={String(numB)}
+          keyboardType="numeric"
+        />
+        <TextInput />
+      </View>
+      <View style={styles.button}>
+        <Button title="+" onPress={addition} />
+        <Button title="-" onPress={subtraction} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +47,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
+  button: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    color: 'red'
+  },
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: 200
+  }
 });
